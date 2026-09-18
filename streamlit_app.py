40960d4ad902d9eff83d3bb43c21663d10369255
kieferpj <23822859@navigators.org>
2026-09-18 15:47:11 -0600

    Update streamlit_app.py
    added f thing?

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,7 @@ if ingredients_list:
         st.subheader(fruit_chosen + ' Nutrition Information')
 
         smoothiefroot_response = requests.get(
-            "https://my.smoothiefroot.com/api/fruit/" + search_on
+            f"https://my.smoothiefroot.com/api/fruit/{search_on}"
         )
 
         sf_df = st.dataframe(
